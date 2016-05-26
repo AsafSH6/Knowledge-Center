2e11d9780a0031392f0707cb7aeb4bc67141d68b
Asaf <asafsemail@gmail.com>
2016-05-26 17:41:15 +0300

    ba

--- routers/index.js
+++ routers/index.js
@@ -4,11 +4,13 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
     //res.sendFile(path.join(__dirname, '../views', 'index.html'));
-    res.render('index.html', {port: process.env.PORT || 5000});
+    //res.render('index.html', {port: process.env.PORT || 5000});
+    res.render('index.html', {port: 80});
 });
 
 router.get('/admin/', function(req, res) {
-    res.render('index-admin.html', {port: process.env.PORT || 5000});
+    //res.render('index-admin.html', {port: process.env.PORT || 5000});
+    res.render('index-admin.html', {port: 80});
 });
 
 router.get('/statistics/', function(req, res) {
