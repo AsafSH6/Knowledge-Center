661456ba10855d5b170e3114e0cca150854e5bcd
Asaf <asafsemail@gmail.com>
2016-05-26 15:49:23 +0300

    port 80

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('./app')
 
-app.listen(8080, 'localhost', function(){
+app.listen(80, 'localhost', function(){
     var host = app.address().address
     var port = app.address().port
 
